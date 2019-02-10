9d3b7c8e1f3a2ef2be83c1051997d29f2422de42
SaiviIslam <saivi.islam123@gmail.com>
2019-02-10 21:15:05 +0100

    Lab 1

--- src/main/java/junit/homwork/README.md
+++ src/main/java/junit/homwork/README.md
@@ -20,13 +20,22 @@ github-repot lämnas in via ITHS Distans.
 
 1. Vilken teknik använde du för att välja testfallen?
 
+jag använde juint för att välja testfallen.
+
 2. Vilka antaganden har du gjort? Det vill säga vilka 
 beslut har du tagit och varför?
+
+jag har tagit beslut för att använda assertEquals method för att jamföra actuella resultatet med det förväntade resultatet.
 
 3. Hittade du några buggar? Alltså något som du anser man behöver 
 undersöka mer innan man kan produktionssätta systemet.
 
+jag hittade en bugg i reverse methoden. jag angav fel förväntade resultat och hade ingen idea varför testet blir fel och sen jag
+tittade på methoden och ansåg att man behöver titta på methoden innan de skriver testfallen.
+
 4. Vad var svårast med laborationen och varför var det svårast?
+
+ det svarast delen av laboration var att hitta buggen.
 
 ## Bedömning
 
---
+++ src/test/java/se/iths/StringUtilityTest.java
@@ -0,0 +1,43 @@
+package se.iths;
+
+import org.junit.Test;
+import junit.homwork.StringUtility;
+import org.junit.Assert;
+
+public class StringUtilityTest {
+    @Test
+    public void camelCase(){
+
+        StringUtility st = new StringUtility();
+        String result = st.camelCase( "Hej jag heter Johan" );
+        Assert.assertEquals(
+                "HejJagHeterJohan",
+                 result
+        );
+    }
+
+    @Test
+    public void reverse(){
+        StringUtility st = new StringUtility();
+        String result = st.reverse("Johan");
+        Assert.assertEquals(
+                "nahoj",
+                result
+        );
+    }
+
+    @Test
+    public void isValidEmailAddress(){
+        StringUtility st = new StringUtility();
+        boolean result = st.isValidEmailAddress("saivi_islam@hotmail.com");
+        Assert.assertEquals(
+                true,
+                result
+        );
+        boolean result1 = st.isValidEmailAddress("saivi_islam@hotmail");
+        Assert.assertEquals(
+                false,
+                result1
+        );
+    }
+}
